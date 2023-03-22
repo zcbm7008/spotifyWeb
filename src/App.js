fb65cd22fd174bfbbb223702e92c7bc6d1494659
zcbm7008 <zcbm7008@gmail.com>
2023-03-22 17:45:28 +0900

    intinity scroll,first load

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import MusicList from "./components/MusicList";
 import useStore from "./store/MusicStore";
+import LoadingSpinner from "./components/LoadingSpinner";
 
 function App() {
   const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
@@ -14,47 +14,87 @@ function App() {
     "user-read-playback-state user-library-modify user-library-read user-top-read";
 
   const [token, setToken] = useState("");
+  const [lastIntersectingItem, setLastIntersectingItem] = useState(null);
+  const [page, setPage] = useState(0);
+  const [loading, setLoading] = useState(false);
+
   const { addMusicList, getMusic } = useStore((state) => state);
   const likes = useStore((state) => state.likeMusicList);
 
+  const ioCallback = (entries, io) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        setPage((prev) => prev + 1);
+        io.unobserve(entry.target);
+      }
+    });
+  };
+
+  const searchPlayLists = async (e) => {
+    if (e) e.preventDefault();
+    console.log(token);
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+    try {
+      const { data } = await axios.get("https://api.spotify.com/v1/me/tracks", {
+        headers,
+        params: {
+          offset: page * 20,
+          limit: 20,
+        },
+      });
+      console.log(data);
+      addMusicList("like", data.items);
+    } catch {
+      console.error("fetching error");
+    }
+    setLoading(false);
+  };
+
   useEffect(() => {
     const hash = window.location.hash;
-    let token = window.localStorage.getItem("token");
+    let localtoken = window.localStorage.getItem("localtoken");
 
-    if (!token && hash) {
+    if (!token && !localtoken && hash) {
       let urlParams = new URLSearchParams(
         window.location.hash.replace("#", "?")
       );
-      token = urlParams.get("access_token");
+      localtoken = urlParams.get("access_token");
       window.location.hash = "";
-      window.localStorage.setItem("token", token);
-      setToken(token);
+      window.localStorage.setItem("localtoken", localtoken);
+      setToken(localtoken);
     }
-    if (token) {
-      window.localStorage.setItem("token", token);
-      setToken(token);
+    if (!token && localtoken) {
+      setToken(localtoken);
     }
-  }, []);
+    if (token) searchPlayLists();
+  }, [token]);
 
   const logout = () => {
     setToken("");
-    window.localStorage.removeItem("token");
+    window.localStorage.removeItem("localtoken");
   };
 
-  const searchPlayLists = async () => {
-    const { data } = await axios.get("https://api.spotify.com/v1/me/tracks", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    console.log(data);
-    addMusicList("like", data.items);
-  };
+  useEffect(() => {
+    let observer;
+    if (lastIntersectingItem) {
+      console.log(lastIntersectingItem);
+      observer = new IntersectionObserver(ioCallback, { threshold: 0.5 });
+      observer.observe(lastIntersectingItem);
+    }
+    return () => observer && observer.disconnect();
+  }, [lastIntersectingItem]);
 
-  if (likes.length === 0) {
-    token && searchPlayLists();
-    console.log("loaded");
-  }
+  useEffect(() => {
+    console.log(`page = ${page}`);
+    if (page >= 1) searchPlayLists();
+  }, [page]);
+
+  // if (likes.length === 0) {
+  //   token && searchPlayLists();
+  //   console.log("loaded");
+  // }
 
   return (
     <div className="App">
@@ -71,7 +111,14 @@ function App() {
         )}
 
         {!token && <h2>Please Login</h2>}
-        {likes && <MusicList likesList={likes} />}
+        {token && (
+          <MusicList
+            likesList={likes}
+            setLastIntersectingItem={setLastIntersectingItem}
+          />
+        )}
+        <LoadingSpinner />
+        {/* <button onClick={searchPlayLists}>Search</button> */}
       </header>
     </div>
   );
--- src/components/MusicList.js
+++ src/components/MusicList.js
@@ -1,19 +1,40 @@
 import MusicItem from "./MusicItem";
 import classes from "./MusicList.module.css";
+import React, { forwardRef } from "react";
 
-const MusicList = (props) => {
+const MusicList = forwardRef((props, ref) => {
   let likes = props.likesList;
+  const { setLastIntersectingItem } = props;
+
+  React.useImperativeHandle(ref, () => ({
+    setLastIntersectingItem,
+  }));
+
   return (
-    <div className={classes.list}>
-      <ul key={likes.key} className={classes.ul}>
-        {likes.map((el, index) => (
-          <li key={el.key} className={classes.li}>
-            <MusicItem like={el} index={index} />
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div className={classes.list}>
+        <ul key={likes.key} className={classes.ul}>
+          {likes.map((el, index) => (
+            <>
+              {index === likes.length - 1 ? (
+                <li
+                  key={el.key}
+                  className={classes.li}
+                  ref={setLastIntersectingItem}
+                >
+                  <MusicItem like={el} index={index} />
+                </li>
+              ) : (
+                <li key={el.key} className={classes.li}>
+                  <MusicItem like={el} index={index} />{" "}
+                </li>
+              )}
+            </>
+          ))}
+        </ul>
+      </div>
+    </>
   );
-};
+});
 
 export default MusicList;
